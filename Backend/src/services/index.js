409800cfe9fb517d491e11dc10e0e698f224b167
MuhammadShahud <64006560+MuhammadShahud@users.noreply.github.com>
2022-03-29 01:29:58 +0500

    Delete Backend directory

--- Backend/src/services/index.js
+++
@@ -1,8 +0,0 @@
-module.exports.authService = require('./auth.service');
-module.exports.userService = require('./user.service');
-module.exports.tokenService = require('./token.service');
-module.exports.productService = require('./product.services');
-module.exports.mailService = require('./mail.service');
-module.exports.questionService = require('./question.service');
-module.exports.paymentService = require('./payment.service');
-module.exports.cardService = require('./card.service');
